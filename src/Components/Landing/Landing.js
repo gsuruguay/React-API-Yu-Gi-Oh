b415589b18971eafcdbb7ec7e8cb3e628fce544e
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-15 01:01:11 -0300

    React-bootstrap components are added to Landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,13 +1,29 @@
 //mport { NavLink } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Col, Image, Navbar, Nav, Row } from 'react-bootstrap';
+import "./Landing.css";
 
 export default function Landing() {
     return (
-        <div>
-            <img src="https://i.postimg.cc/kGWPtHsS/yugioh-home.png" alt="Yu-Gi-Oh"/>
-            <Button>Go home!</Button>
-            <Button>About</Button>
-        </div>
+        <Container fluid className="cont-landing">
+            <Navbar bg="dark" variant="dark" fixed="top">
+                <Container >
+                    <Navbar.Brand href="/"><Image width="120" src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Logo Yu-Gi-Oh" /></Navbar.Brand>
+                    <Nav className="justify-content-end">
+                        <Nav.Link className="ml-1" href="/about">About</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <Row>
+                <Col className="text-center mt-5 pt-5">
+                    <Image src="https://i.postimg.cc/kGWPtHsS/yugioh-home.png" alt="Yu-Gi-Oh" />
+                </Col>
+                <Col className="align-self-center text-center">
+                    <Image src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Titulo Yu-Gi-Oh" />
+
+                    <Button variant="dark" size="lg">Go home!</Button>
+                </Col >
+            </Row>
+        </Container>
     );
 }
 
