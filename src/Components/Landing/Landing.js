525dbacfd3328090b37bf7d9bfa2a3f35f68ff42
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-14 21:42:43 -0300

    Landing component is created

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,13 @@
+//mport { NavLink } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+export default function Landing() {
+    return (
+        <div>
+            <img src="https://i.postimg.cc/kGWPtHsS/yugioh-home.png" alt="Yu-Gi-Oh"/>
+            <Button>Go home!</Button>
+            <Button>About</Button>
+        </div>
+    );
+}
+
