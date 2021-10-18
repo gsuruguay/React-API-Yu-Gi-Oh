5b14a07f727c7bbae54a877bbc86e422d366b101
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-18 13:37:41 -0300

    Function is added to obtain 20 cards

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,38 @@
-//import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from 'react-bootstrap';
 import NavBar from '../NavBar/NavBar';
 import Section from '../Section/Section';
+import cards from "../../cardinfo.json";
+
 //import "./Home.css";
 
 export default function Home() {
 
-    /* const [cardListAPI, setCardListAPI] = useState([]); */
+    const [cardListAPI, setCardListAPI] = useState([]);
+
+    useEffect(() => {
+        setCardListAPI(cards.data)
+    }, [])
+
+    const getListCards = () => {
+        let i = 0;
+        let cardList = [];
+        while (i < 20) {
+            cardList.push(cardListAPI[Math.floor(Math.random() * cardListAPI.length)])
+            i++;
+        }
+        return cardList;
+    }
+
+     /* const limitedListCards = getListCards(); */
+
+    /* console.log(limitedListCards) */
+
 
     return (
         <Container fluid>
             <NavBar />
-            <Section />
+            <Section limitedListCards={getListCards()} />
         </Container>
     );
 }
