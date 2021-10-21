78870caf2cf156fe506969400bc87c5173774b47
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-21 19:31:22 -0300

    Link to Home is added in button

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,29 +1,29 @@
-//mport { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button, Container, Col, Image, Navbar, Nav, Row } from 'react-bootstrap';
 import "./Landing.css";
 
 export default function Landing() {
     return (
-        <Container fluid className="cont-landing">
-            <Navbar bg="dark" variant="dark" fixed="top">
-                <Container >
-                    <Navbar.Brand href="/"><Image width="120" src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Logo Yu-Gi-Oh" /></Navbar.Brand>
-                    <Nav className="justify-content-end">
-                        <Nav.Link className="ml-1" href="/about">About</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <Row>
-                <Col className="text-center mt-5 pt-5">
-                    <Image src="https://i.postimg.cc/kGWPtHsS/yugioh-home.png" alt="Yu-Gi-Oh" />
-                </Col>
-                <Col className="align-self-center text-center">
-                    <Image src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Titulo Yu-Gi-Oh" />
+            <Container fluid className="cont-landing">
+                <Navbar bg="dark" variant="dark" fixed="top">
+                    <Container >
+                        <Navbar.Brand href="/"><Image width="120" src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Logo Yu-Gi-Oh" /></Navbar.Brand>
+                        <Nav className="justify-content-end">
+                            <Nav.Link className="ml-1" href="/about">About</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <Row>
+                    <Col className="text-center mt-5 pt-5">
+                        <Image src="https://i.postimg.cc/kGWPtHsS/yugioh-home.png" alt="Yu-Gi-Oh" />
+                    </Col>
 
-                    <Button variant="dark" size="lg">Go home!</Button>
-                </Col >
-            </Row>
-        </Container>
+                    <Col className="align-self-center text-center">
+                        <Image src="https://i.postimg.cc/sXkRxzYy/yugioh-letras.png" alt="Titulo Yu-Gi-Oh" />
+                        <Link to="/home"><Button variant="dark" size="lg">Go home!</Button></Link>
+                    </Col >
+                </Row>
+            </Container>
     );
 }
 
