c4c1ab46b265ad5857a4112c2cf0e34683f739e4
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-21 19:48:58 -0300

    App is modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Landing from './Components/Landing/Landing';
+import Home from './Components/Home/Home';
+import { Switch, Route } from "react-router-dom";
 import About from './Components/About/About';
 import Details from './Components/DetailsCards/Details';
 import Footer from './Components/Footer/Footer';
@@ -6,8 +9,14 @@ import Contacts from './Components/Contact/Contact';
 function App() {
   return (
     <div>
-      <Contacts/>
-      
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
