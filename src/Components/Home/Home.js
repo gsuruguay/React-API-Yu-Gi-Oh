581cbde36a11f0ce1232b775e18368d6223edb5b
gsuruguay <guillermo.suruguay@gmail.com>
2021-11-04 01:22:50 -0300

    getDataApi() function is added using axios to connect API

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
 import Section from '../Section/Section';
-import cards from "../../cardinfo.json";
 import SideBar from "../SideBar/SideBar";
 import './Home.css';
 
 export default function Home() {
 
+    //Datos de API
+    const [dataAPI, setDataAPI] = useState([]);
+
     //Lista que contiene 40 cards filtradas maximo.
-    const [cardListAPI, setCardListAPI] = useState([]);
+    const [longListCards, setLongListCards] = useState([]);
 
     //Lista que se envia a los componentes con 20 cards maximo.
-    const [filterList, setFilterList] = useState([]);
+    const [smallListCards, setSmallListCards] = useState([]);
 
     //Estado para usar en btn show more Cards del SideBar
     const [showMoreCards, setShowMoreCards] = useState(false);
@@ -23,28 +26,29 @@ export default function Home() {
     //Buscador de NavBar
     const [searchValue, setSearchValue] = useState("");
 
-    //Filtro de Precios
-    /* const [filterEbayPrice, setFilterEbayPrice] = useState({
-        min: "",
-        max: ""
-    }); */
-    /*  const [filterAmazonPrice, setFilterAmazonPrice] = useState({
-            min: "",
-            max: ""
-        }); */
+    //Obtiene los datos de la API
+    useEffect(() => {
+        async function getDataApi() {
+            const resul = await axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php");
+            const resulData = resul.data;
+            setDataAPI(resulData.data);
+        }
+        getDataApi();
+    }, [])
 
     //Funcion que crea la lista random de cards
-    const getRandomCardList = (numLimit = 40, datos = cards.data) => {
+    const getRandomCardList = (numLimit = 40, datos) => {
         let i = 0;
         let cardList = [];
         while (i < numLimit) {
             cardList = [...cardList, datos[Math.floor(Math.random() * datos.length)]];
             i++;
         }
-        setCardListAPI(cardList);
-        setFilterList(cardList.slice(0, 20));
+        setLongListCards(cardList);
+        setSmallListCards(cardList.slice(0, 20));
     }
 
+    //Lista de Race disponibles
     const raceList = ["Aqua", "Beast", "Creator-God", "Cyberse", "Dinosaur", "Divine-Beast", "Dragon", "Fairy", "Fiend", "Fish", "Insect", "Machine", "Plant", "Psychic", "Pyro", "Reptile", "Rock", "Sea Serpent", "Spellcaster", "Thunder", "Warrior", "Winged", "Normal", "Field", "Equip", "Continuous", "Quick-Play", "Ritual", "Counter"]
 
     //Función para ordenar o filtrar las cards
@@ -56,44 +60,44 @@ export default function Home() {
         }
 
         if (e.target.id === "name-asc") {
-            nuevaLista = filterList.sort(sortAsc)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort(sortAsc)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "name-desc") {
-            nuevaLista = filterList.sort(sortDesc)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort(sortDesc)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "race-asc") {
-            nuevaLista = filterList.sort(sortRaceAsc)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort(sortRaceAsc)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "race-desc") {
-            nuevaLista = filterList.sort(sortRaceDesc)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort(sortRaceDesc)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "level-asc") {
-            nuevaLista = filterList.sort((a, b) => a.level - b.level)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => a.level - b.level)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "level-desc") {
-            nuevaLista = filterList.sort((a, b) => b.level - a.level)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => b.level - a.level)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "atk-asc") {
-            nuevaLista = filterList.sort((a, b) => a.atk - b.atk)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => a.atk - b.atk)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "atk-desc") {
-            nuevaLista = filterList.sort((a, b) => b.atk - a.atk)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => b.atk - a.atk)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "def-asc") {
-            nuevaLista = filterList.sort((a, b) => a.def - b.def)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => a.def - b.def)
+            setSmallListCards([...nuevaLista]);
         }
         if (e.target.id === "def-desc") {
-            nuevaLista = filterList.sort((a, b) => b.def - a.def)
-            setFilterList([...nuevaLista]);
+            nuevaLista = smallListCards.sort((a, b) => b.def - a.def)
+            setSmallListCards([...nuevaLista]);
         }
     }
 
@@ -118,18 +122,6 @@ export default function Home() {
         return 0;
     }
 
-    //Funcion del buscador falta mejorar con axios
-    //const searchByName = async (termino) => {
-    /* const response = await axios.get("");
-    const data = response.data; */
-    //axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${termino}`)
-    /* .then(res => {
-      const resulCards = res.data;
-      this.setState({ resulCards });
-      getListCards(20, resulCards);
-    }) */
-    //}
-
     const handleSearch = e => {
         setSearchValue(e.target.value);
     }
@@ -139,19 +131,19 @@ export default function Home() {
         getSearchValue(searchValue);
     }
 
-    //Busca por name o por race desde json en cards
+    //Busca por name o por race en lista completa
     const getSearchValue = (terminoBusqueda) => {
-        let resultadosBusqueda = cards.data.filter((elemento) =>
+        let resultadosBusqueda = dataAPI.filter((elemento) =>
             elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
             || elemento.race.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
         );
-        setCardListAPI(resultadosBusqueda.slice(0, 40));
-        setFilterList(resultadosBusqueda.slice(0, 20));
+        setLongListCards(resultadosBusqueda.slice(0, 40));
+        setSmallListCards(resultadosBusqueda.slice(0, 20));
     }
 
-    //Filtro por race
+    //Funcion para filtrar por race
     const getSearchByRace = (terminoBusqueda) => {
-        let resultadosBusqueda = cards.data.filter((elemento) =>
+        let resultadosBusqueda = dataAPI.filter((elemento) =>
             elemento.race.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
         );
 
@@ -159,73 +151,44 @@ export default function Home() {
         getRandomCardList(50, resultadosBusqueda);
     }
 
-    //Filtro por Ebay Price
-    /* const handleFilterPrice = (e) => {
-        setFilterEbayPrice({
-            ...filterEbayPrice,
-            [e.target.name]: e.target.value
-        })
-    } */
+    //Component will mount
+    useEffect(() => {
+        if (dataAPI.length !== 0) {
+            getRandomCardList(40, dataAPI);
+        }
+    }, [dataAPI])
 
-    /* const getFilterByEbayPrice = () => {
-        filterByEbayPrice();
-    }    */ 
+    //Cambia el estado segun click boton moreCards en SideBar
+    const showMore = () => {
+        setShowMoreCards(!showMoreCards)
+    }
 
-    /* const filterByEbayPrice = () => {
-        let resultadosBusqueda = cards.data.filter((elemento) =>
-            (parseFloat(elemento.card_prices[0].ebay_price) > parseFloat(filterEbayPrice.min) && parseFloat(elemento.card_prices[0].ebay_price) < parseFloat(filterEbayPrice.max))
-        );
-        setCardListAPI(resultadosBusqueda.slice(0, 40));
-        setFilterList(resultadosBusqueda.slice(0, 20));
-    } */
+    //Hace el seguimiento al estado showMoreCards
+    useEffect(() => {
+        showMoreCards ? setSmallListCards(longListCards) : setSmallListCards(longListCards.slice(0, 20));
+    }, [showMoreCards, longListCards])
 
-    /* const getFilterByAmazonPrice = ()=>{
-        filterByAmazonPrice();
-    } */
-    /* const filterByAmazonPrice = () => {
-        let resultadosBusqueda = cards.data.filter((elemento) =>
-            parseFloat(elemento.card_prices[0].amazon_price) > parseInt(filterPrice.min) && parseFloat(elemento.card_prices[0].amazon_price) < parseInt(filterPrice.max)
-        );
-    //Hace un random de los resultados
-    getRandomCardList(50, resultadosBusqueda);
-    }*/
-
-//Component Did mount
-useEffect(() => {
-    getRandomCardList();
-}, [])
-
-//Cambia el estado segun click boton moreCards en SideBar
-const showMore = () => {
-    setShowMoreCards(!showMoreCards)
-}
-
-//Hace el seguimiento al estado showMoreCards
-useEffect(() => {
-    showMoreCards ? setFilterList(cardListAPI) : setFilterList(cardListAPI.slice(0, 20));
-}, [showMoreCards, cardListAPI])
-
-//Funcion para btn Change Cards de SideBar
-const btnChangeCards = () => {
-    setChangeCards(!changeCards)
-}
-useEffect(() => {
-    changeCards ? getRandomCardList(40) : getRandomCardList();
-    setChangeCards(false);
-}, [changeCards])
+    //Funcion para btn Change Cards de SideBar
+    const btnChangeCards = () => {
+        setChangeCards(!changeCards)
+    }
+    useEffect(() => {
+        changeCards && getRandomCardList(40, dataAPI);
+        setChangeCards(false);
+    }, [changeCards, dataAPI])
 
 
-return (
-    <Container fluid className="cont-home">
-        <NavBar handleSearch={handleSearch} searchValue={searchValue} btnSearch={btnSearch} />
-        <Row>
-            <Col md={10}>
-                <Section filterList={filterList} />
-            </Col>
-            <Col xs={{ order: 'first' }}>
-                <SideBar handleChange={handleChange} showMore={showMore} showMoreCards={showMoreCards} btnChangeCards={btnChangeCards} raceList={raceList} /* getFilterByEbayPrice={getFilterByEbayPrice} handleFilterPrice={handleFilterPrice}  *//* getFilterByAmazonPrice={getFilterByAmazonPrice} */ />
-            </Col>
-        </Row>
-    </Container>
-);
+    return (
+        <Container fluid className="cont-home">
+            <NavBar handleSearch={handleSearch} searchValue={searchValue} btnSearch={btnSearch} />
+            <Row>
+                <Col md={10}>
+                    <Section filterList={smallListCards} />
+                </Col>
+                <Col xs={{ order: 'first' }}>
+                    <SideBar handleChange={handleChange} showMore={showMore} showMoreCards={showMoreCards} btnChangeCards={btnChangeCards} raceList={raceList} />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
